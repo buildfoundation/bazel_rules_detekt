e4a7738d9e3ebedba2408f9609fd8a2a9c0178c7
Artur Dryomov <artur.dryomov@gmail.com>
2019-12-10 08:30:16 +0300

    Address review.

--- tests/integration/test_html_report.sh
+++ tests/integration/test_html_report.sh
@@ -7,6 +7,8 @@ OUTPUT_DIR="$(bazelisk info bazel-bin)/tests/integration/"
 bazelisk clean
 bazelisk build //tests/integration:${TARGET}
 
+set -x
+
 test -f "${OUTPUT_DIR}/${TARGET}_detekt_report.html"
 test -f "${OUTPUT_DIR}/${TARGET}_detekt_report.txt"
 test ! -f "${OUTPUT_DIR}/${TARGET}_detekt_report.xml"
--- tests/integration/test_strict_config.sh
+++ tests/integration/test_strict_config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -eou pipefail
 
-set +e
 bazelisk clean
+
+set +e
 bazelisk build //tests/integration:detekt_with_strict_config > /tmp/bazel.log > /dev/null
 BAZEL_EXIT_CODE=$?
 set -e
