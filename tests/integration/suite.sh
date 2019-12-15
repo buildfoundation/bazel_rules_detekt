84e47eb52bac92d67ca73bf257e3d0564b688b1e
Artur Dryomov <artur.dryomov@gmail.com>
2019-12-15 12:22:24 +0300

    Change .bazelrc path.

--- tests/integration/suite.sh
+++ tests/integration/suite.sh
@@ -5,11 +5,11 @@ DIR="tests/integration"
 
 for STRATEGY in "local" "worker"; do
     echo ":: Executing with the [${STRATEGY}] strategy."
-    echo "build --strategy=Detekt=${STRATEGY}" > "${DIR}/.bazelrc"
+    echo "build --strategy=Detekt=${STRATEGY}" > ".bazelrc"
 
     bazelisk clean
 
     time find ${DIR} -type f -name "test_*.sh" -exec bash {} \;
 done
 
-rm "${DIR}/.bazelrc"
+rm ".bazelrc"
