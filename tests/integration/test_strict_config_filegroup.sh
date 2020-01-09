17be0063d7ba8688e9677eea998bae283c3f4b4f
Artur Dryomov <artur.dryomov@gmail.com>
2020-01-09 20:26:43 +0300

    Add filegroup integration tests

---
+++ tests/integration/test_strict_config_filegroup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eou pipefail
+
+echo ":: Target with strict config filegroup should fail."
+
+set +e
+bazelisk build //tests/integration:detekt_with_strict_config_filegroup > /tmp/bazel.log > /dev/null
+BAZEL_EXIT_CODE=$?
+set -e
+
+test BAZEL_EXIT_CODE != 0
---
+++ tests/integration/test_strict_config_filegroup_baseline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eou pipefail
+
+TARGET="detekt_with_strict_config_filegroup_and_baseline"
+OUTPUT_DIR="$(bazelisk info bazel-bin)/tests/integration/"
+
+echo ":: Target with strict config filegroup and baseline should not fail (like the baseline-less does) and should generate text report."
+
+bazelisk build //tests/integration:${TARGET}
+
+set -x
+
+test ! -f "${OUTPUT_DIR}/${TARGET}_detekt_report.html"
+test -f "${OUTPUT_DIR}/${TARGET}_detekt_report.txt"
+test ! -f "${OUTPUT_DIR}/${TARGET}_detekt_report.xml"
