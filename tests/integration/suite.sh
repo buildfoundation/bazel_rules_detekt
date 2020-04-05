ba687cd2321694a3ae6f31403a00943dbf85fe67
Artur Dryomov <artur.dryomov@gmail.com>
2020-04-05 20:49:31 +0300

    Change RC changes in integration tests.

--- tests/integration/suite.sh
+++ tests/integration/suite.sh
@@ -3,9 +3,15 @@ set -eou pipefail
 
 GENERATED_CODE_DIR="tests/integration/src/main/kotlin/generated"
 
+BAZEL_RC=".bazelrc"
+BAZEL_RC_ORIGINAL=".bazelrc.original"
+
+cp "${BAZEL_RC}" "${BAZEL_RC_ORIGINAL}"
+
 for STRATEGY in "local" "worker"; do
     echo ":: Executing with the [${STRATEGY}] strategy."
-    echo "build --strategy=Detekt=${STRATEGY}" >> ".bazelrc"
+    cp "${BAZEL_RC_ORIGINAL}" "${BAZEL_RC}"
+    echo "build --strategy=Detekt=${STRATEGY}" >> "${BAZEL_RC}"
 
     # Generate a bit of code to keep Bazel working instead of pulling from cache to check strategies execution.
     rm -rf "${GENERATED_CODE_DIR}" && mkdir -p "${GENERATED_CODE_DIR}"
@@ -15,3 +21,6 @@ for STRATEGY in "local" "worker"; do
 
     rm -rf "${GENERATED_CODE_DIR}"
 done
+
+cp "${BAZEL_RC_ORIGINAL}" "${BAZEL_RC}"
+rm "${BAZEL_RC_ORIGINAL}"
