181c81bb84bc6ea16584d018dd060e45238ebf95
Artur Dryomov <artur.dryomov@gmail.com>
2020-04-13 22:44:47 +0300

    Add stdout to Detekt output on errors.

--- detekt/wrapper/src/main/kotlin/io/buildfoundation/bazel/detekt/Executable.kt
+++ detekt/wrapper/src/main/kotlin/io/buildfoundation/bazel/detekt/Executable.kt
@@ -11,20 +11,25 @@ internal interface Executable {
     class DetektImpl(private val detekt: Detekt) : Executable {
 
         override fun execute(args: Array<String>): Result {
-            val outputPrinter = PrintStream(Streams.DevNullOutputStream().buffered())
+            val outputBuffer = ByteArrayOutputStream()
+            val outputPrinter = PrintStream(outputBuffer.buffered())
 
-            val errorPrinterBuffer = ByteArrayOutputStream()
-            val errorPrinter = PrintStream(errorPrinterBuffer.buffered())
+            val errorBuffer = ByteArrayOutputStream()
+            val errorPrinter = PrintStream(errorBuffer.buffered())
 
             return try {
                 detekt.execute(args, outputPrinter, errorPrinter)
 
                 Result.Success
             } catch (e: Exception) {
+                outputPrinter.flush()
+
                 e.printStackTrace(errorPrinter)
                 errorPrinter.flush()
 
-                Result.Failure(errorPrinterBuffer.toString(Charset.defaultCharset()))
+                Result.Failure(arrayOf(outputBuffer, errorBuffer).joinToString(separator = "") {
+                    it.toString(Charset.defaultCharset())
+                })
             } finally {
                 outputPrinter.close()
                 errorPrinter.close()
