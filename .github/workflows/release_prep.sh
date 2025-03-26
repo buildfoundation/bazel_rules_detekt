0d010549710e43fcf887933a6e183003f6d7272b
Ben Lee <ben@ben.cm>
2025-03-26 09:12:40 -0700

    Escape back quotes in release_preop

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -26,7 +26,7 @@ bazel_dep(name = "rules_detekt", version = "${TAG:1}")
 
 ## Using WORKSPACE
 
-Paste this snippet into your `WORKSPACE.bazel` file:
+Paste this snippet into your \`WORKSPACE.bazel\` file:
 
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
