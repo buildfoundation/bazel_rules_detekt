a559bab514d5dc2bca57ea988ff9c39a19e46bb4
Ben Lee <ben@ben.cm>
2026-01-30 17:00:09 -0800

    Integrate rules_pmd

--- detekt/wrapper/src/main/java/io/buildfoundation/bazel/detekt/ExecutionUtils.java
+++ detekt/wrapper/src/main/java/io/buildfoundation/bazel/detekt/ExecutionUtils.java
@@ -1,11 +1,8 @@
 package io.buildfoundation.bazel.detekt;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
