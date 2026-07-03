7c246f47e0a5f8f97cf8decc67769b42c4f8ed90
Ben Lee <ben@ben.cm>
2026-07-02 23:31:56 -0700

    Drop support for WORKSPACE

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -12,30 +12,13 @@ TAG=${GITHUB_REF_NAME}
 PREFIX="bazel_rules_detekt-${TAG:1}"
 ARCHIVE="bazel_rules_detekt-$TAG.tar.gz"
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
-SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
-## Using Bzlmod with Bazel 6
+## Using Bzlmod
 
-1. Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
-2. Add to your \`MODULE.bazel\` file:
+Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
 bazel_dep(name = "rules_detekt", version = "${TAG:1}")
 \`\`\`
-
-## Using WORKSPACE
-
-Paste this snippet into your \`WORKSPACE.bazel\` file:
-
-\`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-http_archive(
-    name = "rules_detekt",
-    sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
-    url = "https://github.com/buildfoundation/bazel_rules_detekt/releases/download/${TAG}/${ARCHIVE}",
-)
 EOF
-
-echo "\`\`\`"
